fb4f8053f932bf242ad6264b036f4794406a6b23
gnehs91@nate.com <rh2112us^^>
2021-10-15 02:57:46 +0900

    Third-Setting : Eureka + Gateway + Config(out)

--- src/main/java/com/spreadmat/vendor/VendorApplication.java
+++ src/main/java/com/spreadmat/vendor/VendorApplication.java
@@ -1,11 +1,9 @@
 package com.spreadmat.vendor;
 
-import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@EnableAdminServer
 @EnableEurekaClient
 @SpringBootApplication
 public class VendorApplication {
